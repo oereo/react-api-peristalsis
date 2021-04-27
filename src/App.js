7b55b7d0b148509a1328ba7d15ca2cdc06f14320
oereo <dlstpgns0406@gmail.com>
2021-04-27 18:52:04 +0900

    refactor: delete base App.js and fix App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Navi from './components/Navigation';
+import Header from './components/Header';
+import LoginModal from './components/LoginModal';
+import {Route} from 'react-router-dom';
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const [user, setUser] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="App">
+        <div className="auto-margin">
+          <Route exact path="/">
+            <Header modal={modal}/>
+          </Route>
+
+          {/*<Route exact path="/">*/}
+          {/*  <Navi/>*/}
+          {/*</Route>*/}
+
+          {/*<Route exact path="/login">*/}
+          {/*  <LoginModal setModal={setModal}/>*/}
+          {/*</Route>*/}
+
+      </div>
     </div>
+    </>
   );
 }
 
